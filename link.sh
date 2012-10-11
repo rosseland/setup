3618e4a4ae9dc1400aef5428df57eb1aa6aaa14e
auduny <audun@ytterdal.net>
2012-10-11 22:50:36 +0200

    Installscript

---
+++ link.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+REL=$(pwd |perl -p -e "s|$HOME/||g")
+
+
+ln -si $REL/profile $HOME/.profile
+ln -si $REL/vimrc $HOME/.vimrc
+ln -si $REL/vim $HOME/.vim
+ln -si $REL/gitconfig $HOME/.gitconfig
+ln -si $REL/siegerc $HOME/.siegerc
+
+ln -si $REL/ssh/config $HOME/.ssh/config
+mkdir $HOME/bin
+ln -si $REL/bin/irssi-notify-osx $HOME/bin/irssi-notify-osx
+ln -si $REL/bin/irssi-notify-gnome $HOME/bin/irssi-notify-gnome
+

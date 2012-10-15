27a8c14bf428c32ab52c0d3b415958bb7d317f50
auduny <audun@ytterdal.net>
2012-10-15 09:24:34 +0200

    Added csshrc as well

--- link.sh
+++ link.sh
@@ -5,9 +5,11 @@ REL=$(pwd |perl -p -e "s|$HOME/||g")
 
 ln -si $REL/profile $HOME/.profile
 ln -si $REL/vimrc $HOME/.vimrc
+rm -ri $HOME/vim
 ln -si $REL/vim $HOME/.vim
 ln -si $REL/gitconfig $HOME/.gitconfig
 ln -si $REL/siegerc $HOME/.siegerc
+ln -si $REL/csshrc $HOME/.csshrc
 
 ln -si $REL/ssh/config $HOME/.ssh/config
 mkdir $HOME/bin

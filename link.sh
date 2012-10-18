115e62b9d2e823fd7c65e7bfc097d177930e9c9d
auduny <audun@ytterdal.net>
2012-10-18 20:43:09 +0200

    Added i2csshrc

--- link.sh
+++ link.sh
@@ -10,8 +10,9 @@ ln -si $REL/vim $HOME/.vim
 ln -si $REL/gitconfig $HOME/.gitconfig
 ln -si $REL/siegerc $HOME/.siegerc
 ln -si $REL/csshrc $HOME/.csshrc
+ln -si $REL/i2csshrc $HOME/.i2csshrc
 
-ln -si $REL/ssh/config $HOME/.ssh/config
+ln -si ../$REL/ssh/config $HOME/.ssh/config
 mkdir $HOME/bin
 ln -si $REL/bin/irssi-notify-osx $HOME/bin/irssi-notify-osx
 ln -si $REL/bin/irssi-notify-gnome $HOME/bin/irssi-notify-gnome
